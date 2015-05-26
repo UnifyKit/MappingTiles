9beffc9920af30ddae2c98532db04de6408b4c75
unifykit <unifykit@gmail.com>
2015-05-26 14:41:51 +0800

    Add sphere mercater support

---
+++ MappingTiles/Tile/TileSchemas/BingMapsTileSchema.cs
@@ -0,0 +1,12 @@
+﻿
+namespace MappingTiles
+{
+    public class BingMapsTileSchema: SphericalMercatorTileSchema
+    {
+        public BingMapsTileSchema()
+            : base(TileFormat.Jpeg)
+        {
+            ZoomLevels.RemoveAt(0);     // Bing Maps does't have the signle tile showing the whole world.
+        }
+    }
+}
---
+++ MappingTiles/Tile/TileSchemas/SphericalMercatorScTilehema.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+
+namespace MappingTiles
+{
+    public class SphericalMercatorTileSchema : TileSchema
+    {
+        private const double ScaleFactor = 78271.51696401953125;
+        private const int defaultZoomLevelNumbers = 20;
+        
+        private int numberOfZoomLevels = 20;
+
+        public SphericalMercatorTileSchema() :
+            this(GetZoomLevels(defaultZoomLevelNumbers), TileFormat.Png)
+        {
+        }
+
+        public SphericalMercatorTileSchema(TileFormat format) :
+            this(GetZoomLevels(defaultZoomLevelNumbers), TileFormat.Png)
+        {
+        }
+
+        internal SphericalMercatorTileSchema(IEnumerable<ZoomLevel> zoomLevels, TileFormat format)
+        {
+            TileFormat = format;
+            Crs = "EPSG:3857";
+            foreach (var zoomLevel in zoomLevels)
+            {
+                ZoomLevels.Add(zoomLevel);
+            }
+            BoundingBox = new BoundingBox(-20037508.342789, -20037508.342789, 20037508.342789, 20037508.342789);
+        }
+
+        public int NumberOfZoomLevels
+        {
+            get { return numberOfZoomLevels; }
+            set
+            {
+                if (numberOfZoomLevels != value)
+                {
+                    InitializeZoomLevels();
+                }
+
+                numberOfZoomLevels = value;
+            }
+        }
+
+        private void InitializeZoomLevels()
+        {
+            ZoomLevels.Clear();
+
+            Collection<ZoomLevel> zoomlevels = GetZoomLevels(NumberOfZoomLevels);
+            foreach (var zoomLevel in zoomlevels)
+            {
+                ZoomLevels.Add(zoomLevel);
+            }
+        }
+
+        private static Collection<ZoomLevel> GetZoomLevels(int numberOfZoomLevels)
+        {
+            Collection<ZoomLevel> zoomlevels = new Collection<ZoomLevel>();
+
+            for (int i = 0; i < numberOfZoomLevels; i++)
+            {
+                var resolution = 2 * ScaleFactor / (1 << i);
+                var zoomLevel = new ZoomLevel(resolution, (i + 1).ToString(CultureInfo.InvariantCulture));
+                zoomlevels.Add(zoomLevel);
+            }
+
+            return zoomlevels;
+        }
+    }
+}
