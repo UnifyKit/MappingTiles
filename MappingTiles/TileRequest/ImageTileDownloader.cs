857d4eb3106eaf0c1d2075cea149e6f2b16f624f
unifykit <unifykit@gmail.com>
2015-06-03 17:56:59 +0800

    Add ImageTileDownloader

---
+++ MappingTiles/TileRequest/ImageTileDownloader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MappingTiles
+{
+    public class ImageTileDownloader : TileDownloader
+    {
+        public override void CancelTileDownload(TileInfo tileInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DownloadTile(TileInfo tileInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateTileDownloadPriority(TileInfo tileInfo, int priority)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
