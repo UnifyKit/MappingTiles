2920de1497796cf38c6ad3ff32f21abb0c332487
unifykit <unifykit@gmail.com>
2015-11-24 15:41:47 +0800

    Add TileLayer

---
+++ MappingTiles/Layer/TileLayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MappingTiles
+{
+    public class TileLayer : Layer
+    {
+        protected TileLayer(string id)
+            : base(id)
+        {
+        }
+    }
+}
