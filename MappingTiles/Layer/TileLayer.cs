7f9bd1275b3e3b7fb05d1f8148ebb7e92be11d98
unifykit <unifykit@gmail.com>
2015-11-27 17:10:28 +0800

    Fix a bug that TileFetcher is not existed

--- MappingTiles/Layer/TileLayer.cs
+++ MappingTiles/Layer/TileLayer.cs
@@ -32,9 +32,9 @@ namespace MappingTiles
 
         public override void ViewChanged(UpdateMode updateMode, View view)
         {
-            if (Visible && view.BoundingBox.Area > 0 && _tileFetcher != null && MaxZoomLevel.Resolution > view.ZoomLevel.Resolution && MinZoomLevel.Resolution < view.ZoomLevel.Resolution)
+            if (Visible && view.BoundingBox.Area > 0 && tileSource != null && MaxZoomLevel.Resolution > view.ZoomLevel.Resolution && MinZoomLevel.Resolution < view.ZoomLevel.Resolution)
             {
-                _tileFetcher.ViewChanged(extent, resolution);
+                //_tileFetcher.ViewChanged(extent, resolution);
             }
         }
     }
