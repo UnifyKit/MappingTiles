7dc7221d25d7fee6663b0310f6d6fae12848b7ae
unifykit <unifykit@gmail.com>
2015-11-27 18:00:39 +0800

    Add mapcontrol

---
+++ MappingTiles.Winforms/Map.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MappingTiles.Winforms
+{
+    public class Map : Control
+    {
+        private MapCore mapCore;
+
+        private Bitmap mapBuffer;
+
+
+    }
+}
